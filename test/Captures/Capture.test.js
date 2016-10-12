648455f9a7ccf56352879a5cd62621a2a75b8356
Eric Hole <eric.hole@gmail.com>
2016-10-11 20:00:43 -0400

    test spacing nits.

--- test/Captures/Capture.test.js
+++ test/Captures/Capture.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import {expect} from 'chai';
+import { expect } from 'chai';
 
 import Capture from '../../modules/Captures/Capture';
 
